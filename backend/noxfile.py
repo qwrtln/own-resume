46cbfe8de243e94c192173402346a90b0fbfffd7
Andrzej Wiącek <andrzej.wiacek@qwrtln.nl>
2020-04-26 20:06:42 +0200

    Add task runner (nox) for running tests, formatting and type checking

---
+++ backend/noxfile.py
@@ -0,0 +1,38 @@
+import nox
+
+
+requirements = [
+    "-r",
+    "requirements.txt",
+    "pytest",
+]
+black_args = [
+    "black",
+    "--check",
+    "--diff",
+    ".",
+]
+mypy_args = [
+    "mypy",
+    "resources/",
+    "models",
+    "app.py",
+]
+
+
+@nox.session(reuse_venv=True)
+def tests(session):
+    session.install(*requirements)
+    session.run("pytest")
+
+
+@nox.session(reuse_venv=True)
+def formatting(session):
+    session.install("black")
+    session.run(*black_args)
+
+
+@nox.session(reuse_venv=True)
+def typing_check(session):
+    session.install("mypy")
+    session.run(*mypy_args)
