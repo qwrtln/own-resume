b4862be7b7cbdee00f31b56c589acd8b696f7d76
Andrzej Wiącek <andrzej.wiacek@qwrtln.nl>
2020-04-26 17:50:32 +0200

    Add sample unittest

--- backend/resources/hello.py
+++ backend/resources/hello.py
@@ -7,6 +7,9 @@ from flask_restful import Resource
 class Hello(Resource):
     def get(self) -> Tuple[str, int]:
         ua = request.user_agent
-        if all([ua.browser, ua.version, ua.platform]):
-            return f"Hello, {ua.browser.capitalize()} {ua.version} on {ua.platform.capitalize()}!", 200
+        if all([ua.browser, ua.version, ua.platform]):  # type: ignore
+            return (
+                f"Hello, {ua.browser.capitalize()} {ua.version.split('.')[0]} on {ua.platform.capitalize()}!",  # type: ignore
+                200,
+            )
         return "Hello, mysterious user!", 200
---
+++ backend/tests/resources/test_hello.py
@@ -0,0 +1,53 @@
+import pytest
+
+from app import app
+
+
+@pytest.fixture
+def client():
+    app.config["TESTING"] = True
+    with app.test_client() as client:
+        yield client
+
+
+def test_get(client):
+    rv = client.get("/")
+    assert b"Hello, mysterious user!" in rv.data
+    assert rv.status_code is 200
+
+
+def test_get_chrome_on_windows(client):
+    rv = client.get(
+        "/",
+        headers={
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36"
+        },
+    )
+    assert b"Hello, Chrome 81 on Windows!" in rv.data
+    assert rv.status_code is 200
+
+
+def test_get_safari_on_macos(client):
+    rv = client.get(
+        "/",
+        headers={
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.1 Safari/605.1.15"
+        },
+    )
+    assert b"Hello, Safari 12 on Macos!" in rv.data
+    assert rv.status_code is 200
+
+
+def test_get_firefox_on_linux(client):
+    rv = client.get(
+        "/",
+        headers={
+            "User-Agent": "Mozilla/5.0 (Linux x86_64; rv:75.0) Gecko/20100101 Firefox/75.0"
+        },
+    )
+    assert b"Hello, Firefox 75 on Linux!" in rv.data
+    assert rv.status_code is 200
+
+
+def test_method_not_allowed(client):
+    client.post("/", data={"spam": "eggs"})
