8bc1bebe0d802c0fe54c30549eb6ff8a0dff166f
Andrzej Wiącek <andrzej.wiacek@qwrtln.nl>
2020-04-26 11:57:31 +0200

    Add user agent parsing

--- backend/resources/hello.py
+++ backend/resources/hello.py
@@ -1,8 +1,12 @@
 from typing import Tuple
 
+from flask import request
 from flask_restful import Resource
 
 
 class Hello(Resource):
     def get(self) -> Tuple[str, int]:
-        return "Hello, World!", 200
+        ua = request.user_agent
+        if all([ua.browser, ua.version, ua.platform]):
+            return f"Hello, {ua.browser.capitalize()} {ua.version} on {ua.platform.capitalize()}!", 200
+        return "Hello, mysterious user!", 200
