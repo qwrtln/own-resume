bd82df059b1444f57103477e1c5150b21628cc6d
Marcin Szewczyk <marcin91.szewczyk@gmail.com>
2020-05-05 14:27:44 +0200

    Frontend refactoring

--- frontend/src/resume/ResumeContainer.tsx
+++ frontend/src/resume/ResumeContainer.tsx
@@ -1,44 +1,36 @@
 import React from "react";
 import Resume from "./Resume";
-import {ResumeModel} from "./ResumeModel";
+import { ResumeModel } from "./ResumeModel";
 
 class ResumeContainer extends React.Component<Props, State> {
   state = {
-    resume: undefined
+    resume: undefined,
   };
 
   render() {
+    let resume: any = this.state.resume;
     return (
       <div>
-        {this.renderResume()}
+        {resume ? <Resume basics={resume.basics} work={resume.work} /> : null}
       </div>
     );
   }
 
-  private renderResume() {
-    let resume: any = this.state.resume;
-    return resume ? <Resume basics={resume.basics} work={resume.work}/> : null;
-  }
-
   componentDidMount() {
-    this.getResumeFromRedux();
+    this.getResumeFromServer();
   }
 
-  private getResumeFromRedux(): void {
-    fetch('/api/resume')
-      .then(response => response.json())
-      .then(value => {
-        console.log(value);
-        this.setState({resume: value});
-      })
+  private getResumeFromServer(): void {
+    fetch("/api/resume")
+      .then((response) => response.json())
+      .then((value) => this.setState({ resume: value }));
   }
 }
 
-interface Props {
-}
+interface Props {}
 
 interface State {
-  resume?: ResumeModel
+  resume?: ResumeModel;
 }
 
 export default ResumeContainer;
