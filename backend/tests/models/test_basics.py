4557f08ec54971d69d7f46cb405958ed125b01b6
Andrzej Wiącek <andrzej.wiacek@qwrtln.nl>
2020-05-12 23:24:02 +0200

    Cover all methods of BasicsModel

--- backend/tests/models/test_basics.py
+++ backend/tests/models/test_basics.py
@@ -5,44 +5,43 @@ from .base import TestModelBase
 
 
 class TestBasics(TestModelBase):
+    expected_json = {
+        "name": "He",
+        "summary": "Knows nothing, you know",
+        "email": "un@kn.own",
+    }
+
     def test_jsonifying(self):
-        expected_json = {
-            "name": "He",
-            "summary": "Knows nothing, you know",
-            "email": "un@kn.own",
-        }
-        basics = BasicsModel(*list(expected_json.values()))
-        assert basics.json() == expected_json
+        basics = BasicsModel(*list(self.expected_json.values()))
+        assert basics.json() == self.expected_json
+
+    def test_fetching(self):
+        basics = BasicsModel(*list(self.expected_json.values()))
+        basics.save_to_db()
+        fetched = basics.fetch()
+        assert isinstance(fetched, BasicsModel)
+        assert hasattr(fetched, "email")
+        assert fetched.name == basics.name
 
     def test_adding_basics(self):
-        expected_json = {
-            "name": "He",
-            "summary": "Knows nothing, you know",
-            "email": "un@kn.own",
-        }
-        basics = BasicsModel(*list(expected_json.values()))
+        basics = BasicsModel(*list(self.expected_json.values()))
         basics.save_to_db()
         assert basics in db.session
         fetched = basics.fetch()
-        assert fetched.json() == expected_json
+        assert fetched.json() == self.expected_json
 
     def test_modifying_basics(self):
-        old_json = {
-            "name": "He",
-            "summary": "Knows nothing, you know",
-            "email": "un@kn.own",
-        }
-        expected_json = {
+        new_json = {
             "name": "She",
             "summary": "Knows something",
             "email": "why@do.you",
         }
-        basics = BasicsModel(*list(old_json.values()))
+        basics = BasicsModel(*list(self.expected_json.values()))
         basics.save_to_db()
         assert basics in db.session
 
-        new_basics = BasicsModel(*list(expected_json.values()))
+        new_basics = BasicsModel(*list(new_json.values()))
         new_basics.save_to_db()
         assert new_basics in db.session
         assert basics not in db.session
-        assert new_basics.json() == expected_json
+        assert new_basics.json() == new_json
