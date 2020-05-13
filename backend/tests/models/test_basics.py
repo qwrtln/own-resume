3cb4706888e1dddd337ce1e1a34512c880a36885
Andrzej Wiącek <andrzej.wiacek@qwrtln.nl>
2020-05-13 21:44:38 +0200

    Add pytest markers

--- backend/tests/models/test_basics.py
+++ backend/tests/models/test_basics.py
@@ -1,9 +1,12 @@
+import pytest
+
 from db import db
 from models import BasicsModel
 
 from .base import TestModelBase
 
 
+@pytest.mark.model
 class TestBasics(TestModelBase):
     expected_json = {
         "name": "He",
@@ -12,11 +15,11 @@ class TestBasics(TestModelBase):
     }
 
     def test_jsonifying(self):
-        basics = BasicsModel(*list(self.expected_json.values()))
+        basics = BasicsModel(**self.expected_json)
         assert basics.json() == self.expected_json
 
     def test_fetching(self):
-        basics = BasicsModel(*list(self.expected_json.values()))
+        basics = BasicsModel(**self.expected_json)
         basics.save_to_db()
         fetched = basics.fetch()
         assert isinstance(fetched, BasicsModel)
@@ -24,7 +27,7 @@ class TestBasics(TestModelBase):
         assert fetched.name == basics.name
 
     def test_adding_basics(self):
-        basics = BasicsModel(*list(self.expected_json.values()))
+        basics = BasicsModel(**self.expected_json)
         basics.save_to_db()
         assert basics in db.session
         fetched = basics.fetch()
@@ -36,11 +39,11 @@ class TestBasics(TestModelBase):
             "summary": "Knows something",
             "email": "why@do.you",
         }
-        basics = BasicsModel(*list(self.expected_json.values()))
+        basics = BasicsModel(**self.expected_json)
         basics.save_to_db()
         assert basics in db.session
 
-        new_basics = BasicsModel(*list(new_json.values()))
+        new_basics = BasicsModel(**new_json)
         new_basics.save_to_db()
         assert new_basics in db.session
         assert basics not in db.session
--- backend/tests/models/test_work.py
+++ backend/tests/models/test_work.py
@@ -1,9 +1,12 @@
+import pytest
+
 from db import db
 from models import WorkModel
 
 from .base import TestModelBase
 
 
+@pytest.mark.model
 class TestWork(TestModelBase):
     work_1 = {
         "company": "Gooooogl",
@@ -17,59 +20,63 @@ class TestWork(TestModelBase):
         "summary": "summary2",
     }
 
+    def given_workplace(self, work):
+        workplace = WorkModel(**work)
+        workplace.save_to_db()
+        return workplace
+
     def test_jsonifying(self):
-        basics = WorkModel(*list(self.work_1.values()))
-        assert basics.json() == self.work_1
+        work = WorkModel(**self.work_1)
+        assert work.json() == self.work_1
 
     def test_fetching_empty_works_list(self):
         fetched = WorkModel.fetch_all()
-
         assert len(fetched) == 0
 
     def test_fetching_single_workplace(self):
-        self.givenWorkplace(self.work_1)
+        self.given_workplace(self.work_1)
 
         fetched = WorkModel.fetch_all()
+        workplace = fetched[0]
 
         assert isinstance(fetched, list)
-        assert fetched[0].company == self.work_1.get("company")
-        assert fetched[0].position == self.work_1.get("position")
-        assert fetched[0].summary == self.work_1.get("summary")
+        assert workplace.company == self.work_1["company"]
+        assert workplace.position == self.work_1["position"]
+        assert workplace.summary == self.work_1["summary"]
 
     def test_fetching_multiple_workplaces(self):
-        self.givenWorkplace(self.work_1)
-        self.givenWorkplace(self.work_2)
+        self.given_workplace(self.work_1)
+        self.given_workplace(self.work_2)
 
         fetched = WorkModel.fetch_all()
 
-        assert isinstance(fetched, list)
         assert len(fetched) == 2
 
     def test_fetching_by_company(self):
-        self.givenWorkplace(self.work_1)
-        self.givenWorkplace(self.work_2)
+        self.given_workplace(self.work_1)
+        self.given_workplace(self.work_2)
 
         fetched = WorkModel.find_by_company("company2")
 
         assert isinstance(fetched, WorkModel)
-        assert fetched.summary == self.work_2.get("summary")
+        assert fetched.summary == self.work_2["summary"]
 
     def test_empty_result_for_fetching_by_company(self):
-        self.givenWorkplace(self.work_1)
+        self.given_workplace(self.work_1)
 
         fetched = WorkModel.find_by_company("company2")
 
         assert fetched is None
 
     def test_deleting_workplace(self):
-        workplace = self.givenWorkplace(self.work_1)
+        workplace = self.given_workplace(self.work_1)
 
         workplace.delete_from_db()
 
         assert workplace not in db.session
 
     def test_modifying_workplace(self):
-        workplace = self.givenWorkplace(self.work_1)
+        workplace = self.given_workplace(self.work_1)
 
         workplace.summary = "new summary"
         workplace.save_to_db()
@@ -78,8 +85,3 @@ class TestWork(TestModelBase):
 
         assert len(fetched) == 1
         assert fetched[0].summary == "new summary"
-
-    def givenWorkplace(self, work):
-        workplace = WorkModel(*list(work.values()))
-        workplace.save_to_db()
-        return workplace
